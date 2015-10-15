c2560dc472f5e59fb8feb1ce2477ec4f7470fbeb
Testingnine123 <testingnine123@gmail.com>
2015-10-14 21:30:32 -0400

    Input array of numbers using list and for.

---
+++ array_num.py
@@ -0,0 +1,7 @@
+num_list = list()
+num = raw_input('How many elements in your array? ')
+print 'Enter numbers '
+for i in range(int(num)):
+	n = raw_input('Num:')
+	num_list.append(int(n))
+print num_list
