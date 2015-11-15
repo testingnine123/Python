3a389a7225b64ac553254f8765a37a87c6b9dc28
Testingnine123 <testingnine123@gmail.com>
2015-11-15 18:53:54 -0500

    Follow links and retrieve content of the last link using urllib and BeautifulSoup

---
+++ retrieveTags_insideTags_BeautifulSoup.py
@@ -0,0 +1,31 @@
+import urllib
+from BeautifulSoup import *
+
+inp_url = raw_input('Enter web address:')
+
+#position of the hyperlink from the top
+inp_position = int(raw_input('Enter position:'))
+
+#number of times the links need to be surfed
+inp_count = int(raw_input('Enter count:'))
+
+#loop to parse links depending on the inp_count given by user
+for i in range(inp_count):
+	#read the first link first
+	fhand = urllib.urlopen(inp_url).read()
+	
+	print 'Retrieving link',i,':',inp_url
+	
+	#soupy the html received
+	soup = BeautifulSoup(fhand)
+	
+	#find the list of anchor tags and parse the href content in them so that we can parse the next link
+	tags = soup('a') #tags will be a list
+	
+	#get the href part of the anchor tag along with the contents
+	inp_url = tags[inp_position-1].get('href')
+	inp_content = tags[inp_position-1].contents[0]
+	
+#print the last value stored in inp_content, which is our required value
+print 'Last url parsed was:',inp_url
+print 'The content in the last url was:',inp_content
