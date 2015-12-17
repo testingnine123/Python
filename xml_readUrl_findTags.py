f8f757ecd247d84dfc4e15ad3de8d1789bc6d518
Testingnine123 <testingnine123@gmail.com>
2015-12-16 20:53:42 -0500

    Read a XML web link, and parse the value of a tag, and sum it up.

---
+++ xml_readUrl_findTags.py
@@ -0,0 +1,36 @@
+#Can use http://python-data.dr-chuck.net/comments_42.xml...
+#...for testing. Sum should be 2482
+
+#module for reading urls
+import urllib
+#module for processing XML data
+import xml.etree.ElementTree as ET
+
+lst = list()
+
+#run the code till the user doesn't enter a url
+while True:
+	sum = 0
+	url = raw_input('Enter location: ')
+	if len(url) < 1 : break
+
+	print 'Retrieving',url
+	
+	#opel the url and read the data
+	data = urllib.urlopen(url).read()
+	
+	print 'Retrived',len(data),'characters'
+
+	#process the XML file
+	tree = ET.fromstring(data)
+
+	#make a list of all the "count" tags stored under the "comment" tag
+	counts = tree.findall('.//comment')
+
+	print 'Count:',len(counts)
+
+	#run a loop to sum all the values of the count tage
+	for item in counts:
+		sum = sum + int(item.find('count').text)
+	
+	print 'Sum:',sum
