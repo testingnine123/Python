1a01145b6ec09e086eca68d454a921b1594ad6e1
Testingnine123 <testingnine123@gmail.com>
2015-12-19 20:18:25 -0500

    Read json data, and find out the tags value.

---
+++ json_readData_findTags.py
@@ -0,0 +1,24 @@
+import json
+import urllib
+
+sum = 0
+
+url = raw_input("Enter location (url):")
+
+#"data" type would be a string
+data = urllib.urlopen(url).read()
+
+print "Retrieving", url
+print "Retrieved", len(data), "character"
+
+#"jsonData" type would be a dictionary
+jsonData = json.loads(data)
+
+#print the number of nodes of "comments" tag
+print "Count:",len(jsonData["comments"])
+
+#the "count" tag is under the comments tag
+for items in jsonData["comments"]:
+	sum = sum + int(items["count"])
+
+print "Sum:",sum
